6bcbb8dde6f7d0871334dfdeb9c5eb91403cb249
hongqian-li <lucas.lhqcd@gmail.com>
2023-10-05 21:51:40 +0300

    Step 14 Make the code a bit clearer
    Move all the declarations of the variables to the top of the code (under the main still

--- src/PrinterApp.java
+++ src/PrinterApp.java
@@ -7,35 +7,27 @@ public class PrinterApp {
 
 		//Provide the name of the creator of the Printer App.
 		String creator = "Hongqian Li";
-		System.out.println("This app is made by: " + creator);
 		
-		//Declare two numeric variables named number1 and number2
+		//Move Variable Declarations to the Top
 		int number1 = 5;
 		int number2 = 2;
+		int multiplication = number1 * number2;
+        int addition = number1 + number2;
+        int subtraction = number1 - number2;
+        int division = number1/number2; 
 		
-		//Print the Value of number1
-		System.out.println(number1);
-		
+        //Print the creator name of the App
+		System.out.println("This app is made by: " + creator);
 		//Print "The value of number1 is 5"
 		System.out.println("The value of number1 is " + number1);
 		
 		//Print "The value of number2 is 2"
 		System.out.println("The value of number2 is " + number2);
 		
-		//Declare an integer variable called multiplication
-		int multiplication;
-		
-		//Multiply number1 and number2 and assign the result to the multiplication variable.
-		multiplication = number1 * number2;
-		
 		//Print "[number1] * [number2] = [multiplication]".
         System.out.println(number1 + " * " + number2 + " = " + multiplication);
         
         //Repeat multiplication for addition, division and substraction.
-        int addition = number1 + number2;
-        int subtraction = number1 - number2;
-        int division = number1/number2; 
-        
         System.out.println(number1 + " + " + number2 + " = " + addition);
         System.out.println(number1 + " - " + number2 + " = " + subtraction);
         System.out.println(number1 + " / " + number2 + " = " + division);
