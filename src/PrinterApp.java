b26fb420bc350e0956eefa779b3a2212be97cad3
hongqian-li <lucas.lhqcd@gmail.com>
2023-10-05 21:14:37 +0300

    Step 4-5 Added Creator Name to the app
    Provide the name of the creator of the Printer App.

--- src/PrinterApp.java
+++ src/PrinterApp.java
@@ -4,10 +4,10 @@ public class PrinterApp {
 	public static void main(String[] args) {
 		// Print "Printer App"
 		System.out.println("Printer App");
-		
-		//Print "This app is made by:".
-		System.out.println("This app is made by:");
-		
+
+		//Provide the name of the creator of the Printer App.
+		String creator = "Hongqian Li";
+		System.out.println("This app is made by: " + creator);
 	}
 
 }
