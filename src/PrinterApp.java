2fac3d7e6990c2f729411303f6e2b5718c0479d6
hongqian-li <lucas.lhqcd@gmail.com>
2023-10-05 21:11:00 +0300

    Step 3 Print ”This app is made by:“
    Prepare for the value of the creator variable

--- src/PrinterApp.java
+++ src/PrinterApp.java
@@ -4,7 +4,10 @@ public class PrinterApp {
 	public static void main(String[] args) {
 		// Print "Printer App"
 		System.out.println("Printer App");
-
+		
+		//Print "This app is made by:".
+		System.out.println("This app is made by:");
+		
 	}
 
 }
